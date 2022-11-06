785d68c42e62280e290c1655a39977b798bd2194
Sahil <sahilmangla7@gmail.com>
2022-11-06 19:07:33 +0530

    minor changes

--- index.c
+++ index.c
@@ -3,14 +3,15 @@
 #include <sys/time.h>
 
 #define MAX_SIZE 1000
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
 #define RESET "\x1B[0m"
 
 struct node
 {
     int start, end;
     int len;
+    int occ;
     struct node *suffix;
     struct node *label[26];
 };
@@ -22,6 +23,7 @@ NODE *new ()
     head->start = -1;
     head->end = -1;
     head->len = -1;
+    head->occ = 0;
     head->suffix = NULL;
     for (int i = 0; i < 26; ++i)
     {
@@ -32,8 +34,8 @@ NODE *new ()
 
 NODE *root1, *root2, *current;
 int Suffix[MAX_SIZE + 2];
-int Prefix[MAX_SIZE + 2]= {0};
-int occ[MAX_SIZE + 2];
+int Prefix[MAX_SIZE + 2] = {0};
+NODE *palSuf[MAX_SIZE + 2];
 
 int insert(char *s, int pos)
 {
@@ -46,18 +48,31 @@ int insert(char *s, int pos)
             break;
         cur = cur->suffix;
     }
+
+    if(cur->label[letter] != NULL){
+        current = cur->label[letter];
+        current->occ++;
+        NODE* temp = current->suffix;
+        while(temp!=root1 && temp!=root2){
+            temp->occ++;
+            temp = temp->suffix;
+        }
+        return 0;
+    }
+
     NODE *t = new ();
     t->len = cur->len + 2;
     t->end = pos;
     t->start = pos - t->len + 1;
     cur->label[letter] = t;
-
+    palSuf[pos] = t;
+    cur->label[letter]->occ++;
     if (t->len == 1)
     {
         t->suffix = root2;
         current = t;
-        Suffix[pos] = 1;
         Prefix[pos]++;
+        Suffix[pos] = 1;
         return 0;
     }
 
@@ -70,10 +85,21 @@ int insert(char *s, int pos)
             break;
         }
     }
+    cur = t;
+    while (cur->start != -1)
+    {
+        Prefix[pos - cur->end + cur->start]++;
+        cur = cur->suffix;
+    }
+
     current = t;
     Suffix[pos] = 1 + Suffix[current->suffix->end];
-    Prefix[t->start]++;
-    occ[pos]++;
+
+    NODE* temp = current->suffix;
+    while(temp!=root1 && temp!=root2){
+        temp->occ++;
+        temp = temp->suffix;
+    }
     return 0;
 }
 
@@ -86,6 +112,7 @@ void print(char *s, NODE *head)
             printf("%c", s[i]);
         }
         printf("\n");
+        printf("OCC val : %d\n",head->occ);
     }
     for (int i = 0; i < 26; ++i)
     {
@@ -115,6 +142,41 @@ int countDistinctPalindromes(NODE *head)
     return count + 1;
 }
 
+void pop(int pos, char *s)
+{
+    int letter = s[pos] - 'a';
+    Suffix[pos] = 0;
+    NODE *cur = current;
+    while (cur->start != -1)
+    {
+        Prefix[pos - cur->end + cur->start]--;
+        cur = cur->suffix;
+    }
+
+    if (pos > 0)
+    {
+        cur = palSuf[pos - 1];
+        while (1)
+        {
+            if (pos - 1 - cur->len >= 0 && s[pos - 1 - cur->len] == s[pos])
+                break;
+            cur = cur->suffix;
+        }
+        cur->label[letter] = NULL;
+    }
+
+    free(current);
+    if (pos > 0)
+    {
+        current = palSuf[pos - 1];
+    }
+    else
+    {
+        current = root2;
+    }
+    return;
+}
+
 int count()
 {
     int count1 = countDistinctPalindromes(root1);
@@ -126,7 +188,6 @@ int startAtIndex(int pos)
 {
     return Prefix[pos];
 }
-
 
 int endAtIndex(int pos)
 {
@@ -149,17 +210,19 @@ int main()
     char word[MAX_SIZE];
     char *s = word;
     int i = 0;
+    int size = 0;
 
     while (1)
     {
         printf("\n");
         printf("List: \n");
         printf("1) Press 1 to Insert: \n");
-        printf("2) Press 2 to Print: \n");
-        printf("3) Press 3 to see no of distinct palindromes: \n"); 
-        printf("4) Press 4 to see no of palindromes ending at certain index: \n");
-        printf("5) Press 5 to see no of palindromes starting at certain index: \n");
-        printf("6) Press 6 to exit\n");
+        printf("2) Press 2 for Multipop: \n");
+        printf("3) Press 3 to Print: \n");
+        printf("4) Press 4 to see no of distinct palindromes: \n");
+        printf("5) Press 5 to see no of palindromes ending at certain index: \n");
+        printf("6) Press 6 to see no of palindromes starting at certain index: \n");
+        printf("7) Press 7 to exit\n");
 
         int choice;
         printf("Enter your choice : ");
@@ -174,27 +237,56 @@ int main()
             printf("Append the string : ");
             while ((str = (char)getchar()) != '\n')
             {
-                if(str>='A' && str<='Z'){
+                if (str >= 'A' && str <= 'Z')
+                {
                     str = str + 32;
                 }
-                
+
                 word[i] = str;
                 ++i;
             }
-            for (int j = 0; j < i; ++j)
+            for (int j = size; j < i; ++j)
             {
                 insert(s, j);
             }
+            size = i;
             break;
         }
-
         case 2:
-        {   
+        {
+            printf("Enter the number of times you want to pop : ");
+            int num;
+            scanf("%d", &num);
+            gettimeofday(&t0, NULL);
+
+            for (int j = 0; j < num; j++)
+            {
+                if (i == 0)
+                {
+                    break;
+                }
+                pop(i - 1, s);
+                i--;
+                size--;
+            }
+
+            printf(YEL);
+            printf(RESET);
+            gettimeofday(&t1, NULL);
+            secs = t1.tv_sec - t0.tv_sec;
+            usecs = t1.tv_usec - t0.tv_usec;
+            mtime = ((secs)*1000 + usecs / 1000.0) + 0.5;
+            printf(GRN "Time taken : %lf sec\n", mtime / 100.0);
+            printf(RESET);
+            break;
+        }
+        case 3:
+        {
             gettimeofday(&t0, NULL);
             printf("\nString at the given moment : ");
-            for(int j=0;j<i;j++)
+            for (int j = 0; j < i; j++)
             {
-                printf("%c",word[j]);
+                printf("%c", word[j]);
             }
             printf("\n");
             printf("Distinct Palindromes : \n");
@@ -202,65 +294,71 @@ int main()
             show(s);
             printf(RESET);
             gettimeofday(&t1, NULL);
-            secs  = t1.tv_sec  - t0.tv_sec;
+            secs = t1.tv_sec - t0.tv_sec;
             usecs = t1.tv_usec - t0.tv_usec;
-            mtime = ((secs) * 1000 + usecs/1000.0) + 0.5;
-            printf(GRN "Time taken : %lf sec\n", mtime/100.0);
-            printf(RESET);
-            break;
-        }
-        case 3:
-        {
-            gettimeofday(&t0, NULL);
-            printf("Total number of distinct palindromes : ");
-            printf(YEL); printf("%d\n", count()); printf(RESET);
-            gettimeofday(&t1, NULL);
-            secs  = t1.tv_sec  - t0.tv_sec;
-            usecs = t1.tv_usec - t0.tv_usec;
-            mtime = ((secs) * 1000 + usecs/1000.0) + 0.5;
-            printf(GRN "Time taken : %lf sec\n", mtime/100.0);
+            mtime = ((secs)*1000 + usecs / 1000.0) + 0.5;
+            printf(GRN "Time taken : %lf sec\n", mtime / 100.0);
             printf(RESET);
             break;
         }
         case 4:
         {
-            printf("Enter the index to see number of palindromes ending at that index : ");
-            int index;
-            scanf("%d",&index);
-            if(index>=i){
-                printf("Invalid...Index cannot be greater than length\n");
-                continue;
-            }
-
             gettimeofday(&t0, NULL);
-            printf("Number of Palindromes ending at Index %d are : ",index);
-            printf(YEL); printf("%d\n", endAtIndex(index)); printf(RESET);
+            printf("Total number of distinct palindromes : ");
+            printf(YEL);
+            printf("%d\n", count());
+            printf(RESET);
             gettimeofday(&t1, NULL);
-            secs  = t1.tv_sec  - t0.tv_sec;
+            secs = t1.tv_sec - t0.tv_sec;
             usecs = t1.tv_usec - t0.tv_usec;
-            mtime = ((secs) * 1000 + usecs/1000.0) + 0.5;
-            printf(GRN "Time taken : %lf sec\n", mtime/100.0);
+            mtime = ((secs)*1000 + usecs / 1000.0) + 0.5;
+            printf(GRN "Time taken : %lf sec\n", mtime / 100.0);
             printf(RESET);
             break;
         }
         case 5:
         {
-            printf("Enter the index to see number of palindromes starting at that index : ");
+            printf("Enter the index to see number of palindromes ending at that index : ");
             int index;
-            scanf("%d",&index);
+            scanf("%d", &index);
+            if (index >= i)
+            {
+                printf("Invalid...Index cannot be greater than length\n");
+                continue;
+            }
+
             gettimeofday(&t0, NULL);
-            printf("Number of Palindromes starting at Index %d are : ",index);
-            printf(YEL); printf("%d\n", startAtIndex(index)); printf(RESET);
+            printf("Number of Palindromes ending at Index %d are : ", index);
+            printf(YEL);
+            printf("%d\n", endAtIndex(index));
+            printf(RESET);
             gettimeofday(&t1, NULL);
-            secs  = t1.tv_sec  - t0.tv_sec;
+            secs = t1.tv_sec - t0.tv_sec;
             usecs = t1.tv_usec - t0.tv_usec;
-            mtime = ((secs) * 1000 + usecs/1000.0) + 0.5;
-            printf(GRN "Time taken : %lf sec\n", mtime/100.0);
+            mtime = ((secs)*1000 + usecs / 1000.0) + 0.5;
+            printf(GRN "Time taken : %lf sec\n", mtime / 100.0);
             printf(RESET);
             break;
-            
         }
         case 6:
+        {
+            printf("Enter the index to see number of palindromes starting at that index : ");
+            int index;
+            scanf("%d", &index);
+            gettimeofday(&t0, NULL);
+            printf("Number of Palindromes starting at Index %d are : ", index);
+            printf(YEL);
+            printf("%d\n", startAtIndex(index));
+            printf(RESET);
+            gettimeofday(&t1, NULL);
+            secs = t1.tv_sec - t0.tv_sec;
+            usecs = t1.tv_usec - t0.tv_usec;
+            mtime = ((secs)*1000 + usecs / 1000.0) + 0.5;
+            printf(GRN "Time taken : %lf sec\n", mtime / 100.0);
+            printf(RESET);
+            break;
+        }
+        case 7:
         {
             exit(1);
         }
